1facfcc26fa60973108c360d605cb8a5c2d3b140
Jake <jakerfilut@gmail.com>
2021-11-01 16:53:05 -0600

    commit tree c29f0027c8a1591ae4c05a9e92706e9425ef009c

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() *10) +1;
+console.log(random);
+
+let num3 = 10
+let num4 = 6
+const mod = num3 % num4
+
+let max = Math.max(1,2,3,4,20);
